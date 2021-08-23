11b3d71761eef2ff23ea71d3a037f0f557378f1d
Mateo Espinosa Zarlenga <mateoespinosaz2@gmail.com>
2021-08-23 18:39:17 -0500

    Extending purity score functions to work with any representation shape rather than just vectors.

--- concepts_xai/evaluation/metrics/purity.py
+++ concepts_xai/evaluation/metrics/purity.py
@@ -37,13 +37,16 @@ def concept_purity_matrix(
     in argument c_soft).
 
     :param Or[np.ndarray, List[np.ndarray]] c_soft: Predicted set of "soft"
-        concepts by a concept encoder model applied to the testing data. This
-        argument must be an np.ndarray with shape (n_samples, n_concepts) if
-        all concepts are binary. Otherwise, it is expected to be a list of
-        np.ndarray where the i-th element in the list is an array with shape
-        (n_samples, <cardinality of set of labels for i-th concept>)
-        representing a distribution over all possible labels for the i-th
-        concept.
+        concept representations by a concept encoder model applied to the
+        testing data. This argument must be an np.ndarray with shape
+        (n_samples, ..., n_concepts) where the concept representation may be
+        of any rank as long as the last dimension is the dimension used to
+        separate distinct concept representations. If concepts have distinct
+        array shapes for their representations, then this argument is expected
+        to be a list of `n_concepts` np.ndarrays where the i-th element in the
+        list is an array with shape (n_samples, ...) containing the tensor
+        representation of the i-th concept.
+        Note that in either case we only require that the first dimension.
     :param np.ndarray c_true: Ground truth concept values in one-to-one
         correspondence with concepts in c_soft. Shape must be
         (n_samples, n_concepts).
@@ -63,7 +66,7 @@ def concept_purity_matrix(
 
     :return np.ndarray: a matrix with shape (n_concepts, n_concepts)
         where the (i,j)-th entry specifies the testing AUC of using the i-th
-        concept soft labels to predict the j-th concept.
+        concept soft representations to predict the j-th concept.
     """
     # Start by handling default arguments
     predictor_train_kwags = predictor_train_kwags or {}
@@ -81,10 +84,10 @@ def concept_purity_matrix(
 
     if isinstance(c_soft, np.ndarray):
         # Then, all concepts must be binary
-        assert c_soft.shape == c_true.shape, (
+        assert c_soft.shape[0] == c_true.shape[0], (
             f'Expected a many test soft-concepts as ground truth test '
-            f'concepts. Instead got {c_soft.shape} soft-concepts '
-            f'and {c_true.shape} ground truth test concepts.'
+            f'concepts. Instead got {c_soft.shape[0]} soft-concepts '
+            f'and {c_true.shape[0]} ground truth test concepts.'
         )
         concept_label_cardinality = [2 for _ in range(n_concepts)]
         # And for simplicity and consistency, we will rewrite c_soft as a
@@ -93,7 +96,7 @@ def concept_purity_matrix(
         # test samples.
         new_c_soft = [None for _ in range(n_concepts)]
         for i in range(n_concepts):
-            new_c_soft[i] = np.expand_dims(c_soft[:, i], axis=-1)
+            new_c_soft[i] = np.expand_dims(c_soft[..., i], axis=-1)
         c_soft = new_c_soft
     else:
         # Else, time to infer these values from the given list of soft
@@ -172,8 +175,8 @@ def concept_purity_matrix(
 
     for concept_i in range(n_concepts):
         # Construct a test and training set of features for this concept
-        concept_soft_train_x = c_soft[concept_i][train_indexes, :]
-        concept_soft_test_x = c_soft[concept_i][test_indexes, :]
+        concept_soft_train_x = c_soft[concept_i][train_indexes, ...]
+        concept_soft_test_x = c_soft[concept_i][test_indexes, ...]
 
         for concept_j in range(n_concepts):
             # Let's populate the (i,j)-th entry of our matrix by first training
@@ -236,24 +239,26 @@ def encoder_concept_purity_matrix(
     """
     Computes a concept purity matrix where the (i,j)-th entry represents the
     predictive accuracy of a classifier trained to use the i-th concept's soft
-    labels (as given by the encoder model) to predict the ground truth value of
-    the j-th concept.
+    representation (as given by the encoder model) to predict the ground truth
+    value of the j-th concept.
 
     This process is informally defined only for binary concepts by Mahinpei et
     al.'s in "Promises and Pitfalls of Black-Box Concept Learning Models".
-    Nevertheless, this method supports both binary concepts (given as a 2D
-    matrix output when using the encoder's predict method) or categorical
-    concepts (given as a list of 2D matrices when using the encoder's predict
-    method).
+    Nevertheless, this method supports arbitrarily-shaped concept
+    representations (given as a (n_samples, ..., n_concepts) tensor output when
+    using the encoder's predict method) as well as concepts with different
+    representation shapes (given as a list of n_concepts  tensors with shapes
+    (n_samples, ...) when using the encoder's predict method).
 
     :param skelearn-like Estimator encoder_model: An encoder estimator capable
-        of extracting concepts from a set of features. This estimator may
-        produce a vector of binary concept probabilities for each sample (i.e.,
-        in the case of all concepts being binary) or a list of vectors
-        representing probability distributions over the labels for each concept
-        (i.e., in the case of one or more concepts being categorical).
+        of extracting concept representations from a set of features. For
+        example, this estimator may produce a vector of binary concept
+        probabilities for each sample (i.e., in the case of all concepts being
+        binary) or a list of vectors representing probability distributions over
+        the labels for each concept (i.e., in the case of one or more concepts
+        being categorical).
     :param np.ndarray features: An array of testing samples with shape
-        (n_samples, n_features) used to compute the purity matrix.
+        (n_samples, ...) used to compute the purity matrix.
     :param np.ndarray concepts: Ground truth concept values in one-to-one
         correspondence with samples in features. Shape must be
         (n_samples, n_concepts).
@@ -397,6 +402,11 @@ def oracle_purity_matrix(
             # Let's populate the (i,j)-th entry of our matrix by first training
             # a classifier to predict the ground truth value of concept j using
             # the ground truth labels for concept i.
+            if concept_i == concept_j:
+                # Then this is trivially perfectly predictable
+                result[concept_i, concept_j] = 1
+                continue
+
             # Construct a new estimator for performing this prediction
             estimator = predictor_model_fn(
                 concept_label_cardinality[concept_j]
@@ -452,6 +462,8 @@ def norm_purity_score(
     test_size=0.2,
     norm_fn=lambda x: np.linalg.norm(x, ord='fro'),
     oracle_matrix=None,
+    purity_matrix=None,
+    output_matrices=False,
 ):
     """
     Returns the purity score of the given soft concept representations `c_soft`
@@ -467,13 +479,16 @@ def norm_purity_score(
     relationships between different concept labels.
 
     :param Or[np.ndarray, List[np.ndarray]] c_soft: Predicted set of "soft"
-        concepts by a concept encoder model applied to the testing data. This
-        argument must be an np.ndarray with shape (n_samples, n_concepts) if
-        all concepts are binary. Otherwise, it is expected to be a list of
-        np.ndarray where the i-th element in the list is an array with shape
-        (n_samples, <cardinality of set of labels for i-th concept>)
-        representing a distribution over all possible labels for the i-th
-        concept.
+        concept representations by a concept encoder model applied to the
+        testing data. This argument must be an np.ndarray with shape
+        (n_samples, ..., n_concepts) where the concept representation may be
+        of any rank as long as the last dimension is the dimension used to
+        separate distinct concept representations. If concepts have distinct
+        array shapes for their representations, then this argument is expected
+        to be a list of `n_concepts` np.ndarrays where the i-th element in the
+        list is an array with shape (n_samples, ...) containing the tensor
+        representation of the i-th concept.
+        Note that in either case we only require that the first dimension.
     :param np.ndarray c_true: Ground truth concept values in one-to-one
         correspondence with concepts in c_soft. Shape must be
         (n_samples, n_concepts).
@@ -499,20 +514,38 @@ def norm_purity_score(
         the AUC of an oracle that predicts the value of concept j given the
         ground truth of concept i. If not given, then this matrix will be
         computed using the ground truth concept labels.
+    :param np.ndarray purity_matrix: If given, then this must be a 2D array with
+        shape (n_concepts, n_concepts) such that the (i, j)-th entry represents
+        the AUC of predicting the value of concept j given the soft
+        representation of concept i. If not given, then this matrix will be
+        computed using the purity scores from the input soft representations.
+    :param bool output_matrices: If True then this method will output a tuple
+        (score, purity_matrix, oracle_matrix) containing the computed purity
+        score, purity matrix, and oracle matrix given this function's
+        arguments.
 
-    :returns float: A non-negative float representing the degree to which
-        individual concepts in the given bottleneck encode unnecessary
-        information for other concepts. Higher values mean more impurity and
-        the concepts are considered to be pure if the returned value is 0.
+
+    :returns Or[Tuple[float, np.ndarray, np.ndarray], float]: If output_matrices
+        is False (default behavior) then the output will be a non-negative float
+        representing the degree to which individual concepts in the given
+        bottleneck encode unnecessary information for other concepts. Higher
+        values mean more impurity and the concepts are considered to be pure if
+        the returned value is 0. If output_matrices is True, then the output
+        will be a tuple (score, purity_matrix, oracle_matrix) containing the
+        computed purity score, purity matrix, and oracle matrix given this
+        function's arguments.
     """
     # First compute the predictor soft-concept purity matrix
-    pred_matrix = concept_purity_matrix(
-        c_soft=c_soft,
-        c_true=c_true,
-        predictor_model_fn=predictor_model_fn,
-        predictor_train_kwags=predictor_train_kwags,
-        test_size=test_size,
-    )
+    if purity_matrix is not None:
+        pred_matrix = purity_matrix
+    else:
+        pred_matrix = concept_purity_matrix(
+            c_soft=c_soft,
+            c_true=c_true,
+            predictor_model_fn=predictor_model_fn,
+            predictor_train_kwags=predictor_train_kwags,
+            test_size=test_size,
+        )
 
     # Now infer the concept_label_cardinality vector from the given soft labels
     (n_samples, n_concepts) = c_true.shape
@@ -538,7 +571,10 @@ def norm_purity_score(
 
     # Finally, compute the norm of the absolute difference between the two
     # matrices
-    return norm_fn(np.abs(oracle_matrix - pred_matrix))
+    score = norm_fn(np.abs(oracle_matrix - pred_matrix))
+    if output_matrices:
+        return score, pred_matrix, oracle_matrix
+    return score
 
 
 def encoder_norm_purity_score(
@@ -550,6 +586,8 @@ def encoder_norm_purity_score(
     test_size=0.2,
     norm_fn=lambda x: np.linalg.norm(x, ord='fro'),
     oracle_matrix=None,
+    output_matrices=False,
+    purity_matrix=None,
 ):
     """
     Returns the purity score of the concept representations generated by
@@ -572,7 +610,7 @@ def encoder_norm_purity_score(
         representing probability distributions over the labels for each concept
         (i.e., in the case of one or more concepts being categorical).
     :param np.ndarray features: An array of testing samples with shape
-        (n_samples, n_features) used to compute the purity matrix.
+        (n_samples, ...) used to compute the purity matrix.
     :param np.ndarray concepts: Ground truth concept values in one-to-one
         correspondence with samples in features. Shape must be
         (n_samples, n_concepts).
@@ -598,12 +636,26 @@ def encoder_norm_purity_score(
         the AUC of an oracle that predicts the value of concept j given the
         ground truth of concept i. If not given, then this matrix will be
         computed using the ground truth concept labels.
+    :param np.ndarray purity_matrix: If given, then this must be a 2D array with
+        shape (n_concepts, n_concepts) such that the (i, j)-th entry represents
+        the AUC of predicting the value of concept j given the soft
+        representation generated by the encoder for concept i. If not given,
+        then this matrix will be computed using the purity scores from the input
+        encoder's soft representations.
+    :param bool output_matrices: If True then this method will output a tuple
+        (score, purity_matrix, oracle_matrix) containing the computed purity
+        score, purity matrix, and oracle matrix given this function's
+        arguments.
 
-
-    :returns float: A non-negative float representing the degree to which
-        individual concepts in the given bottleneck encode unnecessary
-        information for other concepts. Higher values mean more impurity and
-        the concepts are considered to be pure if the returned value is 0.
+    :returns Or[Tuple[float, np.ndarray, np.ndarray], float]: If output_matrices
+        is False (default behavior) then the output will be a non-negative float
+        representing the degree to which individual concepts in the given
+        bottleneck encode unnecessary information for other concepts. Higher
+        values mean more impurity and the concepts are considered to be pure if
+        the returned value is 0. If output_matrices is True, then the output
+        will be a tuple (score, purity_matrix, oracle_matrix) containing the
+        computed purity score, purity matrix, and oracle matrix given this
+        function's arguments.
     """
     # Simply use the concept purity metric defined above when given
     # soft concepts as computed by the encoder model
@@ -615,4 +667,6 @@ def encoder_norm_purity_score(
         test_size=test_size,
         norm_fn=norm_fn,
         oracle_matrix=oracle_matrix,
+        purity_matrix=purity_matrix,
+        output_matrices=output_matrices,
     )
