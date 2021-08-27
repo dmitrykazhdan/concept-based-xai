47764ddc9fd0fda5fb4e2b2e7f61220ba099d0cf
Pietro Barbiero <barbiero@tutanota.com>
2021-08-27 10:40:49 +0100

    Update niching metrics

--- concepts_xai/evaluation/metrics/niching.py
+++ concepts_xai/evaluation/metrics/niching.py
@@ -3,42 +3,45 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 
-def niche_completeness(c_pred, y_true, predictor_model):
+def niche_completeness(c_pred, y_true, predictor_model, niches):
     '''
     Computes the niche completeness score for the downstream task
     :param c_pred: Concept data predictions, numpy array of shape (n_samples, n_concepts)
     :param y_true: Ground-truth task label data, numpy array of shape (n_samples, n_tasks)
-    :param predictor_model: sklearn model to use for predicting the task labels from the concept data
+    :param predictor_model: trained decoder model to use for predicting the task labels from the concept data
     :return: Accuracy of predictor_model, evaluated on niches obtained from the provided concept and label data
     '''
     n_tasks = y_true.shape[1]
 
     # train the model
     c_train, c_test, y_train, y_test = train_test_split(c_pred, y_true)
-    predictor_model.fit(c_train, y_train)
-
-    # find concept niches
-    niches = _niche_finding(c_train, y_train)
 
     # compute niche completeness for each task
     niche_completeness_list = []
     for task in range(n_tasks):
         # find niche
         niche = np.zeros_like(c_test)
-        niche[:, niches[:, task] > 0] = niche[:, niches[:, task] > 0]
+        niche[:, niches[:, task] > 0] = c_test[:, niches[:, task] > 0]
 
         # compute task predictions
         y_pred_niche = predictor_model.predict(niche)
+        if predictor_model.__class__.__name__ == 'Sequential':
+            # get class labels from logits
+            y_pred_niche = y_pred_niche > 0
 
         # compute niche accuracy
         # the higher the better (high predictive power of the niche)
         accuracy_niche = accuracy_score(y_test[:, task], y_pred_niche[:, task])
         niche_completeness_list.append(accuracy_niche)
 
-    return np.mean(niche_completeness_list), niche_completeness_list
+    result = {
+        'niche_completeness_mean': np.mean(niche_completeness_list),
+        'niche_completeness': niche_completeness_list,
+    }
+    return result
 
 
-def niche_completeness_ratio(c_pred, y_true, predictor_model):
+def niche_completeness_ratio(c_pred, y_true, predictor_model, niches):
     '''
     Computes the niche completeness ratio for the downstream task
     :param c_pred: Concept data predictions, numpy array of shape (n_samples, n_concepts)
@@ -51,21 +54,24 @@ def niche_completeness_ratio(c_pred, y_true, predictor_model):
 
     # train the model
     c_train, c_test, y_train, y_test = train_test_split(c_pred, y_true)
-    predictor_model.fit(c_train, y_train)
-    y_pred_test = predictor_model.predict(c_test)
 
-    # find concept niches
-    niches = _niche_finding(c_train, y_train)
+    y_pred_test = predictor_model.predict(c_test)
+    if predictor_model.__class__.__name__ == 'Sequential':
+        # get class labels from logits
+        y_pred_test = y_pred_test > 0
 
     # compute niche completeness for each task
     niche_completeness_list = []
     for task in range(n_tasks):
         # find niche
         niche = np.zeros_like(c_test)
-        niche[:, niches[:, task] > 0] = niche[:, niches[:, task] > 0]
+        niche[:, niches[:, task] > 0] = c_test[:, niches[:, task] > 0]
 
         # compute task predictions
         y_pred_niche = predictor_model.predict(niche)
+        if predictor_model.__class__.__name__ == 'Sequential':
+            # get class labels from logits
+            y_pred_niche = y_pred_niche > 0
 
         # compute accuracies
         accuracy_base = accuracy_score(y_test[:, task], y_pred_test[:, task])
@@ -76,10 +82,14 @@ def niche_completeness_ratio(c_pred, y_true, predictor_model):
         niche_completeness = accuracy_niche / accuracy_base
         niche_completeness_list.append(niche_completeness)
 
-    return np.mean(niche_completeness_list), niche_completeness_list
+    result = {
+        'niche_completeness_ratio_mean': np.mean(niche_completeness_list),
+        'niche_completeness_ratio': niche_completeness_list,
+    }
+    return result
 
 
-def niche_purity(c_pred, y_true, predictor_model):
+def niche_purity(c_pred, y_true, predictor_model, niches):
     '''
     Computes the niche purity score for the downstream task
     :param c_pred: Concept data predictions, numpy array of shape (n_samples, n_concepts)
@@ -92,44 +102,51 @@ def niche_purity(c_pred, y_true, predictor_model):
 
     # train the model
     c_train, c_test, y_train, y_test = train_test_split(c_pred, y_true)
-    predictor_model.fit(c_train, y_train)
-
-    # find concept niches
-    niches = _niche_finding(c_train, y_train)
 
     # compute niche completeness for each task
     niche_purity_list = []
+    niche_impurity_list = []
     for task in range(n_tasks):
         # find niche
         niche = np.zeros_like(c_test)
-        niche[:, niches[:, task] > 0] = niche[:, niches[:, task] > 0]
+        niche[:, niches[:, task] > 0] = c_test[:, niches[:, task] > 0]
 
         # find concepts outside the niche
         niche_out = np.zeros_like(c_test)
-        niche_out[:, niches[:, task] <= 0] = niche_out[:, niches[:, task] <= 0]
+        niche_out[:, niches[:, task] <= 0] = c_test[:, niches[:, task] <= 0]
 
         # compute task predictions
         y_pred_niche = predictor_model.predict(niche)
         y_pred_niche_out = predictor_model.predict(niche_out)
+        if predictor_model.__class__.__name__ == 'Sequential':
+            # get class labels from logits
+            y_pred_niche = y_pred_niche > 0
+            y_pred_niche_out = y_pred_niche_out > 0
 
-        # compute accuracies
-        accuracy_niche = accuracy_score(y_test[:, task], y_pred_niche[:, task])
-        accuracy_niche_out = accuracy_score(y_test[:, task], y_pred_niche_out[:, task])
+        # compute purity scores
+        # niche purity: the task accuracy obtained using concepts INSIDE the niche
+        niche_purity = accuracy_score(y_test[:, task], y_pred_niche[:, task])
+        # niche impurity: the task accuracy obtained using concepts OUTSIDE the niche w.r.t.
+        niche_impurity = accuracy_score(y_test[:, task], y_pred_niche_out[:, task])
 
-        # compute the ratio of:
-        # - the task accuracy obtained using concepts OUTSIDE the niche w.r.t.
-        # - the task accuracy obtained using concepts INSIDE the niche
-        # the lower the better (less information leakage among niches)
-        niche_purity = accuracy_niche_out / accuracy_niche
         niche_purity_list.append(niche_purity)
+        niche_impurity_list.append(niche_impurity)
 
-    return np.mean(niche_purity_list), niche_purity_list
+    result = {
+        'niche_purity_mean': np.mean(niche_purity_list),
+        'niche_impurity_mean': np.mean(niche_impurity_list),
+        'niche_purity': niche_purity_list,
+        'niche_impurity': niche_impurity_list,
+    }
+    return result
 
 
-def _niche_finding(c, y, method = 'corr'):
+def niche_finding(c, y, mode='abs', threshold=0.5):
     n_concepts = c.shape[1]
-    if method == 'corr':
-        niches = (np.corrcoef(np.hstack([c, y]).T) > 0)[:n_concepts, n_concepts:]
+    niching_matrix = np.corrcoef(np.hstack([c, y]).T)
+    cy_cov = niching_matrix[:n_concepts, n_concepts:]
+    if mode == 'abs':
+        niches = np.abs(cy_cov) > threshold
     else:
-        niches = (np.corrcoef(np.hstack([c, y]).T) > 0)[:n_concepts, n_concepts:]
-    return niches
+        niches = cy_cov > threshold
+    return niches, cy_cov, niching_matrix
