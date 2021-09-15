6ce70f62dae1d3a71e65161488faca8b4a30dacb
Pietro Barbiero <barbiero@tutanota.com>
2021-09-15 14:42:12 +0100

    Niching metrics w/ AUC

--- concepts_xai/evaluation/metrics/niching.py
+++ concepts_xai/evaluation/metrics/niching.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, roc_auc_score
 from sklearn.model_selection import train_test_split
 
 
@@ -13,28 +13,33 @@ def niche_completeness(c_pred, y_true, predictor_model, niches):
     '''
     n_tasks = y_true.shape[1]
 
-    # train the model
-    c_train, c_test, y_train, y_test = train_test_split(c_pred, y_true)
-
     # compute niche completeness for each task
-    niche_completeness_list = []
+    niche_completeness_list, y_pred_list = [], []
     for task in range(n_tasks):
         # find niche
-        niche = np.zeros_like(c_test)
-        niche[:, niches[:, task] > 0] = c_test[:, niches[:, task] > 0]
+        niche = np.zeros_like(c_pred)
+        niche[:, niches[:, task] > 0] = c_pred[:, niches[:, task] > 0]
 
         # compute task predictions
         y_pred_niche = predictor_model.predict(niche)
         if predictor_model.__class__.__name__ == 'Sequential':
             # get class labels from logits
             y_pred_niche = y_pred_niche > 0
+        elif len(y_pred_niche.shape) == 1:
+            y_pred_niche = y_pred_niche[:, np.newaxis]
 
         # compute niche accuracy
         # the higher the better (high predictive power of the niche)
-        accuracy_niche = accuracy_score(y_test[:, task], y_pred_niche[:, task])
+        accuracy_niche = accuracy_score(y_true[:, task], y_pred_niche[:, task])
         niche_completeness_list.append(accuracy_niche)
 
+        y_pred_list.append(y_pred_niche[:, task])
+
+    y_preds = np.vstack(y_pred_list)
+    auc = roc_auc_score(y_true, y_preds, multi_class='ovo')
+
     result = {
+        'auc_purity': auc,
         'niche_completeness_mean': np.mean(niche_completeness_list),
         'niche_completeness': niche_completeness_list,
     }
@@ -52,30 +57,31 @@ def niche_completeness_ratio(c_pred, y_true, predictor_model, niches):
     '''
     n_tasks = y_true.shape[1]
 
-    # train the model
-    c_train, c_test, y_train, y_test = train_test_split(c_pred, y_true)
-
-    y_pred_test = predictor_model.predict(c_test)
+    y_pred_test = predictor_model.predict(c_pred)
     if predictor_model.__class__.__name__ == 'Sequential':
         # get class labels from logits
         y_pred_test = y_pred_test > 0
+    elif len(y_pred_test.shape) == 1:
+        y_pred_test = y_pred_test[:, np.newaxis]
 
     # compute niche completeness for each task
     niche_completeness_list = []
     for task in range(n_tasks):
         # find niche
-        niche = np.zeros_like(c_test)
-        niche[:, niches[:, task] > 0] = c_test[:, niches[:, task] > 0]
+        niche = np.zeros_like(c_pred)
+        niche[:, niches[:, task] > 0] = c_pred[:, niches[:, task] > 0]
 
         # compute task predictions
         y_pred_niche = predictor_model.predict(niche)
         if predictor_model.__class__.__name__ == 'Sequential':
             # get class labels from logits
             y_pred_niche = y_pred_niche > 0
+        elif len(y_pred_niche.shape) == 1:
+            y_pred_niche = y_pred_niche[:, np.newaxis]
 
         # compute accuracies
-        accuracy_base = accuracy_score(y_test[:, task], y_pred_test[:, task])
-        accuracy_niche = accuracy_score(y_test[:, task], y_pred_niche[:, task])
+        accuracy_base = accuracy_score(y_true[:, task], y_pred_test[:, task])
+        accuracy_niche = accuracy_score(y_true[:, task], y_pred_niche[:, task])
 
         # compute the accuracy ratio of the niche w.r.t. the baseline (full concept bottleneck)
         # the higher the better (high predictive power of the niche)
@@ -100,20 +106,18 @@ def niche_purity(c_pred, y_true, predictor_model, niches):
     '''
     n_tasks = y_true.shape[1]
 
-    # train the model
-    c_train, c_test, y_train, y_test = train_test_split(c_pred, y_true)
-
     # compute niche completeness for each task
     niche_purity_list = []
     niche_impurity_list = []
+    y_pred_list = []
     for task in range(n_tasks):
         # find niche
-        niche = np.zeros_like(c_test)
-        niche[:, niches[:, task] > 0] = c_test[:, niches[:, task] > 0]
+        niche = np.zeros_like(c_pred)
+        niche[:, niches[:, task] > 0] = c_pred[:, niches[:, task] > 0]
 
         # find concepts outside the niche
-        niche_out = np.zeros_like(c_test)
-        niche_out[:, niches[:, task] <= 0] = c_test[:, niches[:, task] <= 0]
+        niche_out = np.zeros_like(c_pred)
+        niche_out[:, niches[:, task] <= 0] = c_pred[:, niches[:, task] <= 0]
 
         # compute task predictions
         y_pred_niche = predictor_model.predict(niche)
@@ -122,17 +126,26 @@ def niche_purity(c_pred, y_true, predictor_model, niches):
             # get class labels from logits
             y_pred_niche = y_pred_niche > 0
             y_pred_niche_out = y_pred_niche_out > 0
+        elif len(y_pred_niche.shape) == 1:
+            y_pred_niche = y_pred_niche[:, np.newaxis]
+            y_pred_niche_out = y_pred_niche_out[:, np.newaxis]
 
         # compute purity scores
         # niche purity: the task accuracy obtained using concepts INSIDE the niche
-        niche_purity = accuracy_score(y_test[:, task], y_pred_niche[:, task])
+        niche_purity = accuracy_score(y_true[:, task], y_pred_niche[:, task])
         # niche impurity: the task accuracy obtained using concepts OUTSIDE the niche w.r.t.
-        niche_impurity = accuracy_score(y_test[:, task], y_pred_niche_out[:, task])
+        niche_impurity = accuracy_score(y_true[:, task], y_pred_niche_out[:, task])
 
         niche_purity_list.append(niche_purity)
         niche_impurity_list.append(niche_impurity)
 
+        y_pred_list.append(y_pred_niche_out[:, task])
+
+    y_preds = np.vstack(y_pred_list)
+    auc = roc_auc_score(y_true, y_preds, multi_class='ovo')
+
     result = {
+        'auc_impurity': auc,
         'niche_purity_mean': np.mean(niche_purity_list),
         'niche_impurity_mean': np.mean(niche_impurity_list),
         'niche_purity': niche_purity_list,
